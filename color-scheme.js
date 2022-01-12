054a4fffe1b85b01fc0175d0d37e077c64b30d39
Aayush Kumar Thakur <aayushkumarthakur1@gmail.com>
2022-01-12 16:55:14 +0530

    Renamed packed-circle color scheme

--- color-scheme.js
+++ color-scheme.js
@@ -1,5 +1,5 @@
 document.querySelector('vega-chart').addEventListener('connect', () => {
-    vega.scheme('packed-circle-default', ['#d8ecfb', '#99d3fd', '#ff9836', '#db6c03', '#feca98'])
+    vega.scheme('packed-circle', ['#d8ecfb', '#99d3fd', '#ff9836', '#db6c03', '#feca98'])
     vega.scheme("scatter-plot", ["#2471A4","#70C2FE","#DB6C04","#8B1680"])
     vega.scheme('waterfall-scheme1', ['#feca98','#ff9836'])
     vega.scheme('waterfall-scheme2', ['#41AFFF','#99D3FD'])
